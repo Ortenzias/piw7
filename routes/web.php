288c67bb9d9a43012ba89ac605eaa7b15c246ed3
Ortenzias <102747547+Ortenzias@users.noreply.github.com>
2022-04-01 22:44:21 -0600

    Update web.php
    Debe de verse que comente //Route::get('uis/formulario/{id}', 'show')->name('show.formulario');
    Y que en la ruta uis/formulario lo use publico

--- routes/web.php
+++ routes/web.php
@@ -28,27 +28,11 @@ Route::get('/', function () {
 
 Route::controller(UisController::class)->group(function () {
     Route::get('uis/index', 'index')->name('inicio.index');
-    //Route::get('uis/formulario', 'show')->name('show.formulario');
-    Route::get('uis/formulario/{id}', 'show')->name('show.formulario');
+    Route::get('uis/formulario', 'show')->name('show.formulario');
+    //Route::get('uis/formulario/{id}', 'show')->name('show.formulario');
     Route::get('uis/formularios', 'showMore')->name('showMore.formularios'); 
     Route::get('uis/eliminar', 'remove')->name('eliminar.remove');
     Route::get('uis/crear', 'create')->name('crear.create');
     Route::get('uis/busqueda','search')->name('busqueda.search');
     Route::get('uis/color','color')->name('color.color');
 });
-
-
-
-
-
-/*
-Esta ruta mas bien debe de ser un boton para que se pueda guardar en PDF
-
-Route::get('uis/guardar', [UisController::class, 'download']);
-
-
-Route::get('uis/imprimir', function () {
-//Y el boton de imprimir debe de redireccionar a una pagina aparte para que lo imprima con la informacion del formulario
-    
-});
-*/
